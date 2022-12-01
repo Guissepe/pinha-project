580749e41426881509dcd532ac081a8c453e3a19
natha ngabriel <nathangabrielvf@gmail.com>
2022-12-01 09:45:39 -0300

    failed to fetch data from prismic

---
+++ pinha/src/pages/pageP.tsx
@@ -0,0 +1,22 @@
+import * as prismic from "@prismicio/client"
+import sm from "../../sm.json"
+import {components} from "../slices"
+import { SliceZone } from "@prismicio/react"
+
+export const getStaticProps = async () => {
+    const client = prismic.createClient(sm.apiEndpoint)
+    const page = await client.getByUID("content", "Content")
+    return{
+        props: {
+            page
+        }
+    }
+}
+
+const Page = ({page}) => {
+    return(
+        <SliceZone slices={page.data.slices} components={components}/>
+    )
+}
+
+export default Page
